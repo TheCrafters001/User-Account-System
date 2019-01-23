46cc4ebd83b88770b6252d0d7c0384da577459b3
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-01-23 17:19:16 -0500

    Made it nicer
    I made is so that it is not yelling at the user!

--- code/login.php
+++ code/login.php
@@ -1,6 +1,6 @@
 <div class="w3-panel w3-pale-red w3-display-container w3-border w3-round">
   <span onclick="this.parentElement.style.display='none'"
   class="w3-button w3-large w3-display-topright">&times;</span>
-  <h3>Hey!</h3>
-  <p>You need to make a account to do that!</p>
+  <h3>Login.</h3>
+  <p>You need to <a href="./signup.php">make a account</a>, or <a href="./index.php">login</a> to do that.</p>
 </div>
