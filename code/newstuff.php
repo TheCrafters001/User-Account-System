776cef7cf3fbc05f05693801eff8976887e12df2
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-01-23 17:24:31 -0500

    Create newstuff.php

---
+++ code/newstuff.php
@@ -0,0 +1,7 @@
+
+<div class="w3-panel w3-pale-green w3-display-container w3-border w3-round">
+  <span onclick="this.parentElement.style.display='none'"
+  class="w3-button w3-large w3-display-topright">&times;</span>
+  <h3>We are adding new things.</h3>
+  <p>We are working to get new things added. The login my go down for a little bit.</p>
+</div>
