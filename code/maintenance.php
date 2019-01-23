19c24ed3c3b0e8063db6eeb55b4640c88893a05d
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-01-23 17:15:50 -0500

    Added maintenance popup
    Added this so if you ever do maintenance to the login, for example, for customization, you can notify your users.

---
+++ code/maintenance.php
@@ -0,0 +1,7 @@
+
+<div class="w3-panel w3-pale-red w3-display-container w3-border w3-round">
+  <span onclick="this.parentElement.style.display='none'"
+  class="w3-button w3-large w3-display-topright">&times;</span>
+  <h3>Maintenance!</h3>
+  <p>We are doing maintenance to the login system. The login may go down. If it does, please be patient.</p>
+</div>
