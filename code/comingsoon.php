eabc49d7a81eababe52ca5cee103d620f36c95da
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-01-23 17:17:32 -0500

    Added Coming Soon Popup
    Allows you to notify your users that you are working on it!

---
+++ code/comingsoon.php
@@ -0,0 +1,6 @@
+<div class="w3-panel w3-pale-green w3-display-container w3-border w3-round">
+  <span onclick="this.parentElement.style.display='none'"
+  class="w3-button w3-large w3-display-topright">&times;</span>
+  <h3>Coming soon!</h3>
+  <p>We are working on a new login system. Please be patient as we are still working on getting it set up.</p>
+</div>
